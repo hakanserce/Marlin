d7c285799bc4b457ea7a13b94aecafaa1976e8cc
Hakan Serce <hakanserce@gmail.com>
2021-12-06 00:05:05 -0800

    remeasured and fixed probe offsets with the current shround

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1181,7 +1181,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -10, -5, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -15, -10 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
